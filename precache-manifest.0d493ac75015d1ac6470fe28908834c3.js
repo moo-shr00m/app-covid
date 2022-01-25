fab255e9b4da8edc34092d94d65e25a1b37f333e
moo-shr00m <servivo.digital@gmail.com>
2022-01-25 16:44:57 +0700

    Updates

---
+++ precache-manifest.0d493ac75015d1ac6470fe28908834c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d0082f48e89b6ed68664e7b915a83d",
+    "url": "/app-covid/index.html"
+  },
+  {
+    "revision": "508a7b43cb49fc7a8971",
+    "url": "/app-covid/static/css/main.ebdd2a13.chunk.css"
+  },
+  {
+    "revision": "d7dc071db379389b0a3c",
+    "url": "/app-covid/static/js/2.ccf307d5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/app-covid/static/js/2.ccf307d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508a7b43cb49fc7a8971",
+    "url": "/app-covid/static/js/main.818ab8df.chunk.js"
+  },
+  {
+    "revision": "19ebe9379a4bdc21e650",
+    "url": "/app-covid/static/js/runtime-main.496dbf62.js"
+  }
+]);
